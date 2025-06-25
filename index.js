e596ee7ad5c2c2ab4d0686f1b7a109024e41dbbd
waynetravis <waynetavis123@gmail.com>
2025-06-25 22:35:20 +0300

    initiate commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const foo = function() {
+return 'bar';
+}
+const add = (a, b) => a + b;
+const double = x => x * 2;
+
+const sum = (a, b) => {
+console.log(a);
+console.log(b);
+return a + b;
+};
+
+const nums = [1, 2, 3];
+const squares = nums.map(num => num ** 2);
+
+const divide = function() {
+    return 2000 / 100;
+};
+
+const square = x => x * x;
